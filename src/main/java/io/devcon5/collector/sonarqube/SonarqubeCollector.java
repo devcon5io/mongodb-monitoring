c00356c876e3d612e2e072df6835d2803668e290
Gerald Mücke <gerald.muecke@devcon5.ch>
2018-03-06 10:38:51 +0100

    adds component stats

--- src/main/java/io/devcon5/collector/sonarqube/SonarqubeCollector.java
+++ src/main/java/io/devcon5/collector/sonarqube/SonarqubeCollector.java
@@ -8,15 +8,15 @@ import io.devcon5.measure.Encoder;
 import io.devcon5.measure.Measurement;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
-import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 
-import java.util.Collection;
-import java.util.Optional;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class SonarqubeCollector extends AbstractVerticle {
 
@@ -40,20 +40,57 @@ public class SonarqubeCollector extends AbstractVerticle {
         vertx.setPeriodic(interval, this::pollStatus);
     }
 
-    private void pollStatus(Long timer) {
-        client.newGetRequest("/api/components/search?qualifiers=BRC").send(this::processResult);
+    @Override
+    public void stop() throws Exception {
+        this.webclient.close();
     }
 
-    private void processResult(AsyncResult<HttpResponse<Buffer>> resp) {
+    private void pollStatus(Long timer) {
+        //projects
+        client.newGetRequest("/api/components/search?qualifiers=BRC,DIR,FIL,TRK,UTS&ps=100000")
+              .send(this::processProjects);
+    }
+
+
+    private void processProjects(AsyncResult<HttpResponse<Buffer>> resp) {
 
         if (resp.succeeded()) {
 
-            System.out.println(resp.result().bodyAsJsonObject().encodePrettily());
+            JsonObject body = resp.result().bodyAsJsonObject();
+            Measurement m = body.getJsonArray("components")
+                                .stream()
+                                .map(JsonObject.class::cast)
+                                .map(o -> o.getString("qualifier"))
+                                .map(this::mapComponentQualifier)
+                                .collect(Collectors.groupingBy(Function.identity(),
+                                        Collectors.reducing(0, o -> 1, Integer::sum)))
+                                .entrySet()
+                                .stream()
+                                .collect(() -> Measurement.builder().name("components"),
+                                        (b, e) -> b.value(e.getKey(), e.getValue()),
+                                        (b1, b2) -> {}).build();
 
-//            vertx.eventBus().publish(Digester.DIGEST_ADDR, encoder.encode(m));
+            vertx.eventBus()
+                 .publish(Digester.DIGEST_ADDR, encoder.encode(m));
         } else {
             resp.cause().printStackTrace();
         }
     }
 
+    private String mapComponentQualifier(String qualifier) {
+        switch (qualifier) {
+            case "TRK":
+                return "projects";
+            case "BRC":
+                return "branches";
+            case "DIR":
+                return "directories";
+            case "FIL":
+                return "files";
+            case "UTS":
+                return "tests";
+            default:
+                return "unknown";
+        }
+    }
 }
